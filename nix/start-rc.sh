ca7919f0f90cfa5c83f26105e4d19829cec53385
Jon Anning <astromoose@gmail.com>
2011-09-15 15:19:15 +0200

    Jon: Cleaned up linux start/stop rc scripts.

--- nix/start-rc.sh
+++ nix/start-rc.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-huburl='http://localhost:4444/grid/register'
+huburl='http://scalzi.is.localnet:4444/grid/register'
 
-java -jar lib/selenium-server-standalone*.jar -role webdriver -hub ${huburl} -port 5556 \
--browser browserName=firefox,version=3.6,firefox_binary=/opt/browsers/firefox36/firefox,maxInstances=1,platform=linux \
--browser browserName=firefox,version=4,firefox_binary=/opt/browsers/firefox4/firefox,maxInstances=1,platform=linux \
--browser browserName=firefox,version=6,firefox_binary=/opt/browsers/firefox6/firefox,maxInstances=1,platform=linux
--browser browserName=chrome,
+java -jar ../lib/selenium-server-standalone*.jar -role webdriver -hub ${huburl} -port 5555 
