557ba68e637c509284cb6b8f74f24c317d5c75ff
Jon Anning <astromoose@gmail.com>
2011-09-15 16:03:36 +0200

    Jon: Updated BrowserName on win startup script

--- win/start-rc.bat
+++ win/start-rc.bat
@@ -1,3 +1,3 @@
 SET huburl="http://scalzi.is.localnet:4444/grid/register"
 
-java -jar ..\lib\selenium-server-standalone*.jar -role webdriver -hub %huburl% -port 5556 -browser "browserName=internet_explorer,version=7,maxInstances=3, platform=WINDOWS"
+java -jar ..\lib\selenium-server-standalone*.jar -role webdriver -hub %huburl% -port 5556 -browser "browserName=internetExplorer,version=7,maxInstances=3, platform=WINDOWS"
