b9064952d24e3729ae3c7de39ebc709bcc43ad31
Jon Anning <astromoose@gmail.com>
2011-09-15 15:49:51 +0200

    Jon: Modified windoze rc start.

--- win/start-rc.bat
+++ win/start-rc.bat
@@ -1,3 +1,3 @@
-huburl = 'http://localhost:4444/grid/register'
+SET huburl="http://scalzi.is.localnet:4444/grid/register"
 
-java -jar lib/selenium-server-standalone*.jar -role webdriver -hub %huburl% -port 5556
+java -jar lib/selenium-server-standalone*.jar -role webdriver -hub %huburl% -port 5556 -browser “browserName=internet_explorer,version=7,maxInstances=3, platform=WINDOWS”
