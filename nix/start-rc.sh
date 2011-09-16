c89e862174bfa78a2b2ef60990e916766aa672d3
Jon Anning <astromoose@gmail.com>
2011-09-16 13:20:38 +0200

    Jon: Added chromedriver options for all startups.

--- nix/start-rc.sh
+++ nix/start-rc.sh
@@ -4,4 +4,5 @@ huburl='http://scalzi.is.localnet:4444/grid/register'
 
 java -jar ../lib/selenium-server-standalone*.jar -role webdriver -hub ${huburl} -port 5555 \
 -browser "browserName=firefox, version=6.0.2, maxInstances=3, platform=linux" \
--browser "browserName=chrome, maxInstances=3, platform=linux"
+-browser "browserName=chrome, maxInstances=3, platform=linux" \
+-Dwebdriver.chrome.driver=../lib/nix64/chromedriver
