4e259c1251ddbfaa2870278d0cc440f39b53875e
Jon Anning <astromoose@gmail.com>
2011-09-16 15:10:31 +0200

    Jon: Sorted out ordering of java options on cmd lines

--- nix/start-rc.sh
+++ nix/start-rc.sh
@@ -2,7 +2,8 @@
 
 huburl='http://scalzi.is.localnet:4444/grid/register'
 
-java -jar ../lib/selenium-server-standalone*.jar -role webdriver -hub ${huburl} -port 5555 \
+java -jar ../lib/selenium-server-standalone*.jar \
+-Dwebdriver.chrome.driver=../lib/chromedrivers/nix64/chromedriver \
+-role webdriver -hub ${huburl} -port 5555 \
 -browser "browserName=firefox, version=6.0.2, maxInstances=3, platform=linux" \
 -browser "browserName=chrome, maxInstances=3, platform=linux" \
--Dwebdriver.chrome.driver=../lib/nix64/chromedriver
--- osx/start-rc.sh
+++ osx/start-rc.sh
@@ -1,7 +1,8 @@
 huburl='http://scalzi.is.localnet:4444/grid/register'
 
-java -jar ../lib/selenium-server-standalone*.jar -role webdriver -hub ${huburl} -port 5555 \
+java -jar ../lib/selenium-server-standalone*.jar \
+-Dwebdriver.chrome.driver=../lib/chromedrivers/osx/chromedriver \
+-role webdriver -hub ${huburl} -port 5555 \
 -browser "browserName=safari, maxInstances=3, platform=mac" \
 -browser "browserName=firefox, maxInstances=3, platform=mac" \
 -browser "browserName=chrome, maxInstances=3, platform=mac" \
--Dwebdriver.chrome.driver=../lib/chromedrivers/osx/chromedriver
