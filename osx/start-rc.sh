5f3fcc7cbea85db91565b047487a35ef6c0869a7
Jon of Anning <jon@heinlein.(none)>
2011-09-16 19:44:06 +0200

    Jon: Fixing hub startups again.

--- osx/start-rc.sh
+++ osx/start-rc.sh
@@ -3,6 +3,6 @@ huburl='http://scalzi.is.localnet:4444/grid/register'
 java -jar ../lib/selenium-server-standalone*.jar \
 -Dwebdriver.chrome.driver=../lib/chromedrivers/osx/chromedriver \
 -role webdriver -hub ${huburl} -port 5555 \
--browser "browserName=safari, maxInstances=3, platform=mac" \
--browser "browserName=firefox, maxInstances=3, platform=mac" \
--browser "browserName=chrome, maxInstances=3, platform=mac" \
+-browser "browserName=safari,maxInstances=3,platform=MAC" \
+-browser "browserName=firefox,maxInstances=3,platform=MAC" \
+-browser "browserName=chrome,maxInstances=3,platform=MAC" \
