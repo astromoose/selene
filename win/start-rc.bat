9310aacf57ab0ae691d10b59621a7aa082505a33
Jon Anning <astromoose@gmail.com>
2011-09-16 11:16:55 +0200

    Jon: Fixed typo in *nix startup and added firefox browser to win startup.

--- win/start-rc.bat
+++ win/start-rc.bat
@@ -1,3 +1,3 @@
 SET huburl="http://scalzi.is.localnet:4444/grid/register"
 
-java -jar ..\lib\selenium-server-standalone*.jar -role webdriver -hub %huburl% -port 5556 -browser "browserName=internetExplorer, version=8, maxInstances=3, platform=win7"
+java -jar ..\lib\selenium-server-standalone*.jar -role webdriver -hub %huburl% -port 5556 -browser "browserName=internetExplorer, version=8, maxInstances=3, platform=win7" -browser "browserName=firefox, version=6.0.2, maxInstances=3, platform=win7"
