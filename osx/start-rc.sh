4b7df187cbb37c7c1eb5b8cb115199b6d8fea67c
Jon Anning <astromoose@gmail.com>
2011-09-16 13:04:14 +0200

    Jon: Added chromedriver libs for all platforms.

--- osx/start-rc.sh
+++ osx/start-rc.sh
@@ -1,6 +1,6 @@
 huburl='http://scalzi.is.localnet:4444/grid/register'
 
 java -jar ../lib/selenium-server-standalone*.jar -role webdriver -hub ${huburl} -port 5555 \
--browser "browserName=safari, maxInstances=3, platform=osx" \
--browser "browserName=firefox, maxInstances=3, platform=osx" \
--browser "browserName=chrome, maxInstances=3, platform=osx"
+-browser "browserName=safari, maxInstances=3, platform=mac" \
+-browser "browserName=firefox, maxInstances=3, platform=mac" \
+-browser "browserName=chrome, maxInstances=3, platform=mac"
