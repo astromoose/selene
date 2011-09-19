51c8877aff740c911891680317b3b92d3bcec315
Jon Anning <astromoose@gmail.com>
2011-09-19 15:50:43 +0200

    Jon: Forgot to add ff binary paths in the rc startup for linux.

--- nix/start-rc.sh
+++ nix/start-rc.sh
@@ -6,7 +6,7 @@ java -jar ../lib/selenium-server-standalone*.jar \
 -Dwebdriver.chrome.driver=../lib/chromedrivers/nix32/chromedriver \
 -role webdriver -hub ${huburl} -port 5555 \
 -browser "browserName=firefox,version=6.0.2,maxInstances=3,platform=LINUX" \
--browser "browserName=firefox,version=3.6,maxInstances=3,platform=LINUX" \
--browser "browserName=firefox,version=7.0,maxInstances=3,platform=LINUX" \
+-browser "browserName=firefox,version=3.6,firefox_binary=/opt/ff3/firefox,maxInstances=3,platform=LINUX" \
+-browser "browserName=firefox,version=7.0,firefox_binary=/opt/ff7/firefox,maxInstances=3,platform=LINUX" \
 -browser "browserName=chrome,maxInstances=3,platform=LINUX" \
 -browser "browserName=opera,version=11.51,maxInstances=3,platform=LINUX"
