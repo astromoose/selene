d35e684d000ec26f2d670030100604b179b73f14
Jon Anning <astromoose@gmail.com>
2011-09-19 15:41:22 +0200

    Jon: Added ff 3.6 and 7.0 to firefox on linux rc.

--- nix/start-rc.sh
+++ nix/start-rc.sh
@@ -6,5 +6,7 @@ java -jar ../lib/selenium-server-standalone*.jar \
 -Dwebdriver.chrome.driver=../lib/chromedrivers/nix32/chromedriver \
 -role webdriver -hub ${huburl} -port 5555 \
 -browser "browserName=firefox,version=6.0.2,maxInstances=3,platform=LINUX" \
+-browser "browserName=firefox,version=3.6,maxInstances=3,platform=LINUX" \
+-browser "browserName=firefox,version=7.0,maxInstances=3,platform=LINUX" \
 -browser "browserName=chrome,maxInstances=3,platform=LINUX" \
--browser "browserName=opera,maxInstances=3,platform=LINUX"
+-browser "browserName=opera,version=11.51,maxInstances=3,platform=LINUX"
