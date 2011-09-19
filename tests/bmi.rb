9db938f6d56d8516d39c81da93328a1d65d9445e
Jon Anning <astromoose@gmail.com>
2011-09-19 16:06:03 +0200

    Jon: New rc file for ie9 rc.

--- tests/bmi.rb
+++ tests/bmi.rb
@@ -2,16 +2,43 @@
 
 require 'rubygems'
 require 'selenium-webdriver'
+require 'awesome_print'
+require 'peach'
 
-#hub = 'http://scalzi.is.localnet:4444/wd/hub'
-hub = 'http://localhost:4444/wd/hub'
-browser = :firefox
+hub = 'http://scalzi.is.localnet:4444/wd/hub'
 
-driver = Selenium::WebDriver.for :remote, :url => hub, :desired_capabilities => browser
+# Browser defs
+wie6 = Selenium::WebDriver::Remote::Capabilities.ie(:takes_screenshot => true, :version => '6', :platform => 'WINDOWS')
+wie7 = Selenium::WebDriver::Remote::Capabilities.ie(:takes_screenshot => true, :version => '7', :platform => 'WINDOWS')
+wie8 = Selenium::WebDriver::Remote::Capabilities.ie(:takes_screenshot => true, :version => '8', :platform => 'WINDOWS')
+wie9 = Selenium::WebDriver::Remote::Capabilities.ie(:takes_screenshot => true, :version => '9', :platform => 'WINDOWS')
+wff6 = Selenium::WebDriver::Remote::Capabilities.firefox(:takes_screenshot => true, :version => '6.0.2', :platform => 'WINDOWS')
+wff7 = Selenium::WebDriver::Remote::Capabilities.firefox(:takes_screenshot => true, :version => '7.0', :platform => 'WINDOWS')
+wchr = Selenium::WebDriver::Remote::Capabilities.chrome(:takes_screenshot => true, :platform => 'WINDOWS')
+lff6 = Selenium::WebDriver::Remote::Capabilities.firefox(:takes_screenshot => true, :version => '6.0.2', :platform => 'LINUX')
+lff3 = Selenium::WebDriver::Remote::Capabilities.firefox(:takes_screenshot => true, :version => '3.6', :platform => 'LINUX', :firefox_binary => '/opt/ff3/firefox')
+lff7 = Selenium::WebDriver::Remote::Capabilities.firefox(:takes_screenshot => true, :version => '7.0', :platform => 'LINUX', :firefox_binary => '/opt/ff7/firefox')
+lchr = Selenium::WebDriver::Remote::Capabilities.chrome(:takes_screenshot => true, :platform => 'LINUX', :chrome_binary => '/usr/bin/google-chrome')
+lopr = Selenium::WebDriver::Remote::Capabilities.opera(:takes_screenshot => true, :platform => 'LINUX')
+mff6 = Selenium::WebDriver::Remote::Capabilities.firefox(:takes_screenshot => true, :platform => 'MAC')
+mchr = Selenium::WebDriver::Remote::Capabilities.chrome(:takes_screenshot => true, :platform => 'MAC')
 
-driver.get "http://www.bmivisualizer.com"
+browsers = [ wie8, wff6, wchr, lff3, lff6, lff7, lchr, lopr,  mff6, mchr ]
 
-puts "Page title is #{driver.title}"
-driver.save_screenshot("./#{browser}.png")
+browsers.peach do |browser|
+  begin
+    driver = Selenium::WebDriver.for(
+    	:remote,
+    	:url => hub,
+    	:desired_capabilities => browser
+    )
 
-driver.quit
+    puts "Requested properties: #{driver.capabilities.browser_name.capitalize}(#{driver.capabilities.version}) on #{driver.capabilities.platform.capitalize}."
+    driver.get "http://www.bmivisualizer.com"
+    driver.save_screenshot("./#{driver.capabilities.platform}-#{driver.browser}(#{driver.capabilities.version}).png")
+		driver.quit
+  rescue
+		puts "Problem with #{browser.browser_name} (#{browser.browser.version}) on #{browser.platform}."
+    next
+  end
+end
