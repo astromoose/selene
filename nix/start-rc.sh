cf13a9e97180d0c678cbec2786555e730214836a
Jon Anning <astromoose@gmail.com>
2011-09-19 13:28:44 +0200

    Jon: Changed from 64 to 32bit chromedriver on nix.

--- nix/start-rc.sh
+++ nix/start-rc.sh
@@ -3,7 +3,7 @@
 huburl='http://scalzi.is.localnet:4444/grid/register'
 
 java -jar ../lib/selenium-server-standalone*.jar \
--Dwebdriver.chrome.driver=../lib/chromedrivers/nix64/chromedriver \
+-Dwebdriver.chrome.driver=../lib/chromedrivers/nix32/chromedriver \
 -role webdriver -hub ${huburl} -port 5555 \
 -browser "browserName=firefox,version=6.0.2,maxInstances=3,platform=LINUX" \
 -browser "browserName=chrome,maxInstances=3,platform=LINUX" \
